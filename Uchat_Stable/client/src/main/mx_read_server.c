6e59510f6d642e00f8be6fe81947bbdc8b9a1ccb
D3C0D1N3 <mrozek.yura3@gmail.com>
2021-01-20 19:48:07 +0200

    awdawd

--- Uchat_Stable/client/src/main/mx_read_server.c
+++ Uchat_Stable/client/src/main/mx_read_server.c
@@ -2,7 +2,7 @@
 
 void *mx_read_server(void *arg) {
     client_t *cli = (client_t *)arg;
-    char buff_in[1024];
+    char buff_in[8460];
     int rlen = 0;
 
     while(read(cli->sockfd, &rlen, 4) > 0) {
@@ -16,7 +16,7 @@ void *mx_read_server(void *arg) {
         printf("\n\n<< Recieved %d bytes:\n\n%s\n\n<<", rlen, buff_in);
         mx_json_manager(buff_in, cli);
         gdk_threads_leave();
-        bzero(buff_in, 1024);
+        bzero(buff_in, 8460);
     }
 
     close(cli->sockfd);
