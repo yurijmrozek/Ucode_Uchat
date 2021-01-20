caabf1ccfe43ebddb188488143ceaf19cf1317fa
D3C0D1N3 <mrozek.yura3@gmail.com>
2021-01-20 19:41:07 +0200

    awdaw

--- Uchat_Stable/client/src/gui/message/mx_valid_msg.c
+++ Uchat_Stable/client/src/gui/message/mx_valid_msg.c
@@ -5,10 +5,6 @@ bool mx_valid_msg(gchar *message, client_t *cli) {
 
     if (strlen(message) < 3 || strlen(message) > 255)
         valid = false;
-    for (int i = 0; message[i]; i++) {
-        if (message[i] < 0 || message[i] > 120)
-            valid = false;
-    }
     if (!valid) {
         mx_sending_invite_dialog(cli, 'm');
         return false;
